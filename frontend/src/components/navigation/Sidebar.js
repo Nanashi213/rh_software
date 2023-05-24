67bac30ab63909dca64f62d0236d60ca01f40ec2
SebasA <104111947+Nanashi213@users.noreply.github.com>
2023-05-24 16:06:28 -0500

    entrega

--- frontend/src/components/navigation/Sidebar.js
+++ frontend/src/components/navigation/Sidebar.js
@@ -1,60 +1,80 @@
-import React from 'react';
-import { Col, Dropdown } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Col, Dropdown, Button,Row} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function Sidebar() {
+  const [isHidden, setIsHidden] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsHidden(!isHidden);
+  }
+
   return (
-        <Col xs={2} className="ps-3 pe-3 overflow-auto vh-100 bg-dark text-white d-flex flex-column justify-content-center align-items-center "  fixed='start'>
-          <div fixed='start'>
-            <Dropdown >
-              <Dropdown.Toggle variant="dark" id="dropdown-basic">
-              Modulo 1 
-              </Dropdown.Toggle>
+    <> 
+    {/* 
+    <Row>
+      <Col xs={2} className="d-flex justify-content-center">
+        <Button onClick={toggleSidebar} variant='dark' className='rounded-0'>
+          {isHidden ? "Mostrar Menu" : "Ocultar Menu"}
+        </Button>
+      </Col>
+    </Row>
+    */}
+      <Button onClick={toggleSidebar} variant='dark' className='rounded-0'>
+          {isHidden ? "Mostrar Menu" : "Ocultar Menu"}
+      </Button>
+      <Col xs={2}>
+        <div  fixed='start'  className={`ps-3 pe-3 overflow-auto vh-100 bg-dark text-white d-flex flex-column justify-content-center align-items-center ${isHidden ? 'd-none' : ''}`}>
+          <Dropdown >
+            <Dropdown.Toggle variant="dark" id="dropdown-basic">
+            Modulo 1 
+            </Dropdown.Toggle>
 
-              <Dropdown.Menu className="bg-dark text-white">
-                <Dropdown.Item as={Link} to="/main/joboffers" className="text-white">Ofertas de trabajo</Dropdown.Item>
-                <Dropdown.Item as={Link} to="/contratacion" className="text-white">Contratación</Dropdown.Item>
-                <Dropdown.Item as={Link} to="/option1-3" className="text-white">Option 1-3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+            <Dropdown.Menu className="bg-dark text-white">
+              <Dropdown.Item as={Link} to="/main/joboffers" className="text-white">Ofertas de trabajo</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/contratacion" className="text-white">Contratación</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/option1-3" className="text-white">Option 1-3</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
 
-            <Dropdown >
-              <Dropdown.Toggle variant="dark" id="dropdown-basic">
-              Modulo 2
-              </Dropdown.Toggle>
+          <Dropdown >
+            <Dropdown.Toggle variant="dark" id="dropdown-basic">
+            Modulo 2
+            </Dropdown.Toggle>
 
-              <Dropdown.Menu className="bg-dark text-white">
-                <Dropdown.Item as={Link} to="/option2-1" className="text-white">Option 2-1</Dropdown.Item>
-                <Dropdown.Item as={Link} to="/option2-2" className="text-white">Option 2-2</Dropdown.Item>
-                <Dropdown.Item as={Link} to="/option2-3" className="text-white">Option 2-3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+            <Dropdown.Menu className="bg-dark text-white">
+              <Dropdown.Item as={Link} to="/option2-1" className="text-white">Option 2-1</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/option2-2" className="text-white">Option 2-2</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/option2-3" className="text-white">Option 2-3</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
 
-            <Dropdown >
-              <Dropdown.Toggle variant="dark" id="dropdown-basic">
-              Modulo 3
-              </Dropdown.Toggle>
+          <Dropdown >
+            <Dropdown.Toggle variant="dark" id="dropdown-basic">
+            Modulo 3
+            </Dropdown.Toggle>
 
-              <Dropdown.Menu className="bg-dark text-white">
-                <Dropdown.Item as={Link} to="/option3-1" className="text-white">Option 3-1</Dropdown.Item>
-                <Dropdown.Item as={Link} to="/option3-2" className="text-white">Option 3-2</Dropdown.Item>
-                <Dropdown.Item as={Link} to="/option3-3" className="text-white">Option 3-3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+            <Dropdown.Menu className="bg-dark text-white">
+              <Dropdown.Item as={Link} to="/option3-1" className="text-white">Option 3-1</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/option3-2" className="text-white">Option 3-2</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/option3-3" className="text-white">Option 3-3</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
 
-            <Dropdown >
-              <Dropdown.Toggle variant="dark" id="dropdown-basic">
-              Modulo 4
-              </Dropdown.Toggle>
+          <Dropdown >
+            <Dropdown.Toggle variant="dark" id="dropdown-basic">
+            Modulo 4
+            </Dropdown.Toggle>
 
-              <Dropdown.Menu className="bg-dark text-white">
-                <Dropdown.Item as={Link} to="/option4-1" className="text-white">Option 4-1</Dropdown.Item>
-                <Dropdown.Item as={Link} to="/option4-2" className="text-white">Option 4-2</Dropdown.Item>
-                <Dropdown.Item as={Link} to="/option4-3" className="text-white">Option 4-3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-        </Col>
+            <Dropdown.Menu className="bg-dark text-white">
+              <Dropdown.Item as={Link} to="/option4-1" className="text-white">Option 4-1</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/option4-2" className="text-white">Option 4-2</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/option4-3" className="text-white">Option 4-3</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </Col>
+    </>
   );
 }
 
--- frontend/src/containers/pages/Module_1.jsx
+++ frontend/src/containers/pages/Module_1.jsx
@@ -3,31 +3,47 @@ import Navbar from "../../components/navigation/Navbar.js";
 import Sidebar from "../../components/navigation/Sidebar.js";
 import Footer from "../../components/navigation/Footer.js";
 import JobForm from "../../components/module_1/JobForm.js";
-import React from "react";
+import JobList from "../../components/module_1/Joblist.js";
+import React, { useState } from "react";
 
-import {Row, Col} from "react-bootstrap";
+import {Row, Col, Button} from "react-bootstrap";
 
 function Module_1(){
-    const location = useLocation();
+const location = useLocation();
+const [activeComponent, setActiveComponent] = useState('form');
 
-    if(location.pathname === '/main/joboffers'){
-        return (
-            <>
+const showForm = () => setActiveComponent('form');
+const showList = () => setActiveComponent('list');
+
+
+if(location.pathname === '/main/joboffers'){
+  return (
+    <>
+        <Row >
+            <Navbar />
+        </Row>
+        <Row>
+            <Sidebar />
+        <Col>
+            <Row className="mb-5">
+            <Col className="d-flex justify-content-center">
+                <Button variant="dark" className="rounded-0" onClick={showList}>Mostrar Ofertas</Button>
+                <Button variant="dark" className="rounded-0" onClick={showForm}>Agregar</Button>
+
+            </Col>
+            </Row>
             <Row >
-                <Navbar />
+                {activeComponent === 'form' && <JobForm/>}
+                {activeComponent === 'list' && <JobList/>}
             </Row>
-            <Row>
-                <Sidebar />
-                <Col>
-                <JobForm/>
-                </Col>
-            </Row>
-            <Row>
-                <Footer />
-            </Row>
-            </>
-        )
-    }
+        </Col>
+        </Row>
+        <Row>
+            <Footer />
+        </Row>
+    </>
+  )
+}
 }
 
 export default Module_1;
